145f0c8bb78a83d84d2cdeaa80f4e77e6b767021
AlexSaxena <saxenaalex@gmail.com>
2023-05-11 16:24:35 +0200

    Imports & use calculator

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const calc = require('./routes/calc');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/calculator', calc);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
